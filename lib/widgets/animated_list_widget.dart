14d700aed829a575c272051cb1acab12b65e87d9
Rahman <faiz199011@gmail.com>
2022-10-27 15:19:33 +0430

    Refactroing function

--- lib/widgets/animated_list_widget.dart
+++ lib/widgets/animated_list_widget.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class AnimatedListWidget extends StatefulWidget {
@@ -12,18 +11,43 @@ class _AnimatedListWidgetState extends State<AnimatedListWidget> {
 
   final GlobalKey<AnimatedListState> _key = GlobalKey();
 
-  final TextEditingController _textEditingController = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
 
   final item = [];
 
-  void addItem(String text) {
+  Future<void> addDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: ((context) {
+        return AlertDialog(
+          title: const Text('Add ToDo List'),
+          content: TextField(
+            controller:  _controller,
+            decoration: const InputDecoration(
+              hintText: 'E.g Meetings and Events',
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                addItem(_controller.text);
+                Navigator.pop(context);
+              },
+              child: const Text('Save'),
+            )
+          ],
+        );
+      })
+    );
+  }
 
-    item.insert(0, text, );
+  void addItem(String title) {
+    item.insert(0, title);
     _key.currentState!.insertItem(0, duration: const Duration(seconds: 1));
   }
 
-  void remove(int index, BuildContext context) {
-    AnimatedList.of(context).removeItem(index, (_, animation) {
+  void removeItem(int index, BuildContext context) {
+    AnimatedList.of(context).removeItem(index, (context, animation) {
       return FadeTransition(
         opacity: animation,
         child: SizeTransition(
@@ -36,58 +60,31 @@ class _AnimatedListWidgetState extends State<AnimatedListWidget> {
             ),
             child: ListTile(
               title: Text(item[index], style: const TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w500)),
-              subtitle: Text(DateTime.now().toString().substring(0,10), style: const TextStyle(fontSize: 12,)),
+              subtitle: Text(DateTime.now().toString().substring(0,10), style: const TextStyle(fontSize: 12)),
               trailing: IconButton(
-                onPressed: () => remove(index, context),
-                icon: const Icon(Icons.delete)
+                onPressed: () => removeItem(index, context),
+                icon: const Icon(Icons.delete),
               ),
             ),
           ),
         ),
       );
-    },);
+    });
 
-    item.removeAt(index);
-  }
-
-  Future<void> dialogAddItem(BuildContext context) async {
-    return showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text('Add Todo List'),
-          content : TextField(
-            controller: _textEditingController,
-            decoration: const InputDecoration(
-              hintText: "E.g Home Work"
-            ),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                addItem(_textEditingController.text);
-                Navigator.pop(context);
-              },
-              child: const Text('Save')
-            )
-          ],
-        );
-      }
-    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animated Todo List ')
+        title: const Text('Animated Todo List'),
       ),
-      body : Container(
+      body: Padding(
         padding: const EdgeInsets.all(16),
         child: AnimatedList(
           key: _key,
           initialItemCount: item.length,
-          itemBuilder: ((context, index, animation) {
+          itemBuilder: (((context, index, animation) {
             return FadeTransition(
               opacity: animation,
               child: SizeTransition(
@@ -101,23 +98,22 @@ class _AnimatedListWidgetState extends State<AnimatedListWidget> {
                   ),
                   child: ListTile(
                     title: Text(item[index], style: const TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.w500)),
-                    subtitle: Text(DateTime.now().toString().substring(0,10), style: const TextStyle(fontSize: 12,)),
+                    subtitle: Text(DateTime.now().toString().substring(0,10), style: const TextStyle(fontSize: 12)),
                     trailing: IconButton(
-                      onPressed: () => remove(index, context),
-                      icon: const Icon(Icons.delete)
+                      onPressed: () => removeItem(index, context),
+                      icon: const Icon(Icons.delete),
                     ),
                   ),
                 ),
-              ),
-            );
-          }),
-        ) ,
+            ));
+          })),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          _textEditingController.clear();
-          dialogAddItem(context);
-        } ,
+          _controller.clear();
+          addDialog(context);
+        },
         child: const Icon(Icons.add),
       ),
     );
